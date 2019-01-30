74d4e71e541f332cf396de06029d65c6aaf4798a
visualstudio <visualstudio@122-PC35>
2019-01-30 10:45:32 -0500

    week4...;

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,13 +8,19 @@ namespace ConsoleApp1
 {
     class Program
     {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("hello");
+        }
         class Village
         {
             // Node is an ADT
             // what KIND OF DATA we need?
             public Village nextVillage;
             public String VillageName;
-            public bool IsAstrildHere = false
+            public bool IsAstrildHere = false;
         }
+          
+       
     }
 }
